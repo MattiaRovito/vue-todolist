892f507ab7a02fb15b37bff99706ed016af9cf16
MattiaR <rovitomattia91@gmail.com>
2021-06-23 16:06:21 +0200

    add html + css

---
+++ js/script.js
@@ -0,0 +1,29 @@
+
+//TODO Stampare in pagina un item per ogni elemento contenuto in un array
+
+
+//TODO Ogni item ha una "x" associata: cliccando su di essa, l'item viene rimosso dalla lista
+
+
+//TODO Predisporre un input per aggiungere un nuovo item alla lista: digitando il tasto invio oppure ciccando su un pulsante, il testo digitato viene aggiunto alla lista 
+
+
+
+const app = new Vue ({
+
+    el : "#app",
+
+    
+    data :
+    {
+
+    },
+
+
+    methods :
+    {
+
+    }
+
+
+});
